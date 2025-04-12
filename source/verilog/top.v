c6a2aca3f06e2516d62eda6a12b35e4cb6fb40f5
Aden Briano <adenbriano@Adens-MacBook-Pro-5.local>
2025-04-12 01:11:59 -0500

    created top module

--- source/verilog/top.v
+++ source/verilog/top.v
@@ -1,54 +1,66 @@
-// Templated from my Homework 3 Problem 3 - change as needed
+
 //-----------------------------------------------
-// Combination Lock Integration - lock_top.v
+// Snake Game Integration - top.v
 //-----------------------------------------------
-module lock_top (in_clka, in_clkb, in_restart, in_load_pattern, in_load_test, in_enter, in_pattern, in_test, out_match, out_error, out_state,
-  out_same_sig, out_save_pat_sig, out_save_pat_temp_sig, out_save_test_sig, out_save_test_temp_sig);
+module snake_top (in_clka, in_clkb, in_restart, in_led_array_flat, in_from_logic, in_direction_in, out_game_state, out_direction_state, 
+  out_execution_state, out_to_logic, out_row_cathode, out_column_cathode, in_random_num, out_request_rand);
 
 //----------Input Ports--------------------------
-input wire        in_clka, in_clkb, in_restart, in_load_pattern, in_load_test, in_enter;
-input wire [1:0]  in_pattern, in_test;
+input wire        in_clka, in_clkb, in_restart;
+input wire [1:0]  in_from_logic;
+input wire [63:0] in_led_array_flat;
+input wire [3:0]  in_direction_in;
+input wire [5:0]  in_random_num;
 
 //----------Output Ports-------------------------
-output wire       out_match, out_error, out_same_sig, out_save_pat_sig, out_save_pat_temp_sig, out_save_test_sig, out_save_test_temp_sig;
-output wire [2:0] out_state;
+output wire       out_request_rand;
+output wire [1:0] out_game_state, out_direction_state, out_execution_state;, out_to_logic;
+output wire [7:0] row_cathode, column_cathode;
 
 //----------Internal Variables-------------------
-wire same_sig, save_pat_sig, save_pat_temp_sig, save_test_sig, save_test_temp_sig; // Used to link the FSM controller and datapath
-assign out_same_sig = same_sig;                     // Bring control signals out
-assign out_save_pat_sig = save_pat_sig;             // "
-assign out_save_pat_temp_sig = save_pat_temp_sig;   // "
-assign out_save_test_sig = save_test_sig;           // "
-assign out_save_test_temp_sig = save_test_temp_sig; // "
+wire [1:0] direction_state, to_logic, from_logic; // Used to link the FSM controller and datapath
+wire [63:0] led_array_flat;
+wire [5:0] random_num;
+wire request_rand;
+
+assign out_direction_state = direction_state;       // Bring control signals out
+assign out_to_logic = to_logic;                     // "
+assign in_from_logic = from_logic;                  // "
+assign in_led_array_flat = led_array_flat;          // "
+assign in_random_num = random_num;                  // "
+assign out_request_rand = request_rand;             // "
 
 //----------Code starts here---------------------------------------------------
-lock_fsm fsm (.clka (in_clka),
-              .clkb (in_clkb),
-              .restart (in_restart),
-              .load_pattern (in_load_pattern),
-              .load_test (in_load_test),
-              .enter (in_enter),
-              .same_sig (same_sig),
-              .save_pat_sig (save_pat_sig),
-              .save_pat_temp_sig (save_pat_temp_sig),
-              .save_test_sig (save_test_sig),
-              .save_test_temp_sig (save_test_temp_sig),
-              .match (out_match),
-              .error (out_error),
-              .state (out_state)
-              );
+snake_controller controller (.clka (in_clka),
+                             .clkb (in_clkb),
+                             .restart (in_restart),
+                             .direction_in (in_direction_in),
+                             .from_logic (from_logic),
+                             .led_array_flat (led_array_flat),
+                             .game_state (out_game_state),
+                             .direction_state (direction_state),
+                             .execution_state (out_execution_state),
+                             .to_logic (out_row_cathode),
+                             .column_cathode (out_column_cathode)
+                            );
 
-lock_dp dp (.clka (in_clka),
-            .clkb (in_clkb),
-            .restart (in_restart),
-            .save_pat (save_pat_sig),
-            .save_pat_temp (save_pat_temp_sig),
-            .save_test (save_test_sig),
-            .save_test_temp (save_test_temp_sig),
-            .pattern (in_pattern),
-            .test (in_test),
-            .same (same_sig)
-            );
+snake_logic logic (.clka (in_clka),
+                   .clkb (in_clkb),
+                   .restart (in_restart),
+                   .from_controller (to_logic)),
+                   .direction_state (direction_state),
+                   .random_num (random_num),
+                   .to_controller (from_logic),
+                   .led_array_flat (led_array_flat),
+                   .request_rand (request_rand)
+                  );
+
+snake_prng prng (.clka (in_clka), 
+                 .clkb (in_clkb), 
+                 .restart (in_restart), 
+                 .request_rand (request_rand), 
+                 .random_num (random_num)
+                );
 
 
 
